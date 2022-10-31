7c4832f6ee2c0e21563c58b0a4b447dd8b013dc1
loc-giang <locbgiang@gmail.com>
2022-10-30 20:19:05 -0700

    comments

--- 31-40/35. Search Insert Position.py
+++ 31-40/35. Search Insert Position.py
@@ -8,18 +8,18 @@ Input: nums = [1,3,5,6], target = 5
 Output: 2
 '''
 
-def searchInsert(nums, target):
-    left = 0
-    right = len(nums) - 1
-    while left <= right:
-        mid = (left+right)//2
-        if (target == nums[mid]):
-            return mid
-        elif (target > nums[mid]):
-            left = mid + 1
+def searchInsert(nums, target):             # binary search to get O(log(n))
+    left = 0                                # left pointer
+    right = len(nums) - 1                   # right pointer
+    while left <= right:                    # run loop while left <= than right
+        mid = (left+right)//2               # find mid
+        if (target == nums[mid]):           # if target = mid then return mid
+            return mid                      
+        elif (target > nums[mid]):          # else if target > mid
+            left = mid + 1                  # move left to mid + 1
         else:
-            right = mid - 1
-    return left
+            right = mid - 1                 # move right to mid - 1
+    return left                         # if there is no target, return left
 
 nums = [1,3,5,6]
 target = 5
