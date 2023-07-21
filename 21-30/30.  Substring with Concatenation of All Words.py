3c74c6f7d212e135c4382ffcba1aea1fea585c82
loc-giang <locbgiang@gmail.com>
2023-07-20 23:46:32 -0700

    30 added comments

--- 21-30/30.  Substring with Concatenation of All Words.py
+++ 21-30/30.  Substring with Concatenation of All Words.py
@@ -24,31 +24,34 @@ We return an empty array.
 '''
 
 def findSubstring(s, words):
-    lenWord = len(words[0])
-    lenAllWords = lenWord * len(words)
-    wordsCounter = {}
+    lenWord = len(words[0])             # length of each word in words
+    lenAllWords = lenWord * len(words)  # total length of all the words combined
+    wordsCounter = {}                   # memory, count each words
     answer = []
 
-    for word in words:
+    # first we create a memory that count each word and asign a numeric value
+    for word in words:  
         if word in wordsCounter:
             wordsCounter[word] += 1
         else:
             wordsCounter[word] = 1
     
-    for i in range(len(s)-lenAllWords+1):
-        cutS = s[i:i+lenAllWords]
-        cutSCounter = {}
+    # then we iterate through the string s
+    for i in range(len(s)-lenAllWords+1):   # no point in going beyond this because the answer cant be smaller than lenAllWords
+        cutS = s[i:i+lenAllWords]           # cut the string
+        cutSCounter = {}                    # memory of the words count in the cut string
         
-        for j in range(0, lenAllWords, lenWord):
-            cutword = cutS[j:j+lenWord]
-            if cutword in cutSCounter:
+        for j in range(0, lenAllWords, lenWord):    # here we cut the string into further word length
+            cutword = cutS[j:j+lenWord]             # working with the word now
+            if cutword in cutSCounter:              # same idea as before, add all the words into memory
                 cutSCounter[cutword] += 1
             else:
                 cutSCounter[cutword] = 1
-        if wordsCounter == cutSCounter:
-            answer.append(i)
-    print(answer)       
-    return
+        # out of the inner forloop, now we compare the two counter        
+        if wordsCounter == cutSCounter:     
+            answer.append(i)  # if they are the same we save i, the first position
+
+    return answer  # return asnwer after running through the forloop
 
 
 s = 'barfoothefoobarman'
