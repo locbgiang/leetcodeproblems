9441a504b6f84301191457e006d84bfe201e0af1
loc-giang <locbgiang@gmail.com>
2023-08-30 18:51:21 -0700

    76

--- 71-80/76. Minimum Window Substring.py
+++ 71-80/76. Minimum Window Substring.py
@@ -20,12 +20,44 @@ Output: ""
 Explanation: Both 'a's from t must be included in the window.
 Since the largest window of s only has one 'a', return empty string.
 '''
+
+from itertools import count
+
+
 def minWindow(s, t):
-    for size in range(len(t), len(s)+1):
-        for start in range(0, len(s)-size+1):
-            found = True
-            cut = [*s[start:start+size]]
-            for letter in t:
+    if t == '':
+        return ''
+    
+    countT, window = {}, {}
+    for c in t:
+        countT[c] = 1 + countT.get(c, 0)
+    
+    have, need = 0, len(countT)
+    res, resLen = [-1,-1], float('infinity')
+    l = 0
+    for r in range(len(s)):
+        c = s[r]
+        window[c] = 1 + window.get(c, 0)
+        print(window)
+        if c in countT and window[c] == countT[c]:
+            have += 1
+        
+        while have == need:
+            if(r-l+1) < resLen:
+                res = [l, r]
+                resLen = (r-l+1)
+            window[s[l]] -= 1
+            if s[l] in countT and window[s[l]] < countT[s[l]]:
+                have -= 1
+            l += 1
+    l, r = res
+    return s[l:r+1] if resLen != float('infinity') else ''
+s = "ADOBECODEBANC"
+t = "ABC"
+print(minWindow(s, t))
+
+'''
+for letter in t:
                 if letter not in cut:
                     found = False
                     break
@@ -33,8 +65,4 @@ def minWindow(s, t):
                     cut.remove(letter)
             if found:
                 return s[start:start+size]
-    return ""
-
-s = "ADOBECODEBANC"
-t = "ABC"
-minWindow(s, t)
+'''
