00b04b4d3935bc894eb0b195052442ff05c58695
loc-giang <locbgiang@gmail.com>
2023-10-06 00:37:14 -0700

    128 comment

--- 1-1000/101-200/121-130/128. Longest Consecutive Sequence.py
+++ 1-1000/101-200/121-130/128. Longest Consecutive Sequence.py
@@ -15,13 +15,14 @@ def longestConsecutive(nums):
     longest = 0
     num_set = set(nums)
     for num in nums:
-        if num-1 not in num_set:
-            temp = 1
-            while num+1 in num_set:
-                temp += 1
+        if num - 1 not in num_set:
+            count = 1
+            while num + 1 in num_set:
+                count += 1
                 num += 1
-            longest = max(longest, temp)
+            longest = max(longest, count)
     return longest
+    
 
 nums = [100,4,200,1,3,2]
 longestConsecutive(nums)
