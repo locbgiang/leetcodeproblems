30c6f23c255f31e8aeb30160114710947393014a
loc-giang <locbgiang@gmail.com>
2023-10-15 18:41:18 -0700

    150

---
+++ 1-1000/101-200/141-150/150. Evaluate Reverse Polish Notation.py
@@ -0,0 +1,55 @@
+'''
+You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+Evaluate the expression. Return an integer that represents the value of the expression.
+Note that:
+The valid operators are '+', '-', '*', and '/'.
+Each operand may be an integer or another expression.
+The division between two integers always truncates toward zero.
+There will not be any division by zero.
+The input represents a valid arithmetic expression in a reverse polish notation.
+The answer and all the intermediate calculations can be represented in a 32-bit integer.
+ 
+Example 1:
+Input: tokens = ["2","1","+","3","*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+
+Example 2:
+Input: tokens = ["4","13","5","/","+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6
+
+Example 3:
+Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+Output: 22
+Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+= ((10 * (6 / (12 * -11))) + 17) + 5
+= ((10 * (6 / -132)) + 17) + 5
+= ((10 * 0) + 17) + 5
+= (0 + 17) + 5
+= 17 + 5
+= 22
+'''
+def evalRPN(tokens):
+    signs = ['+', '-', '/', '*']
+    temp = []
+
+    for i in range(len(tokens)):
+        if tokens[i] in signs:
+            a = int(temp.pop())
+            b = int(temp.pop())
+            if tokens[i] == '+':
+                new = a + b
+            elif tokens[i] == '-':
+                new = b - a
+            elif tokens[i] == '/':
+                new = b/a
+            elif tokens[i] == '*':
+                new = b*a
+            temp.append(new)
+        else:
+            temp.append(tokens[i])
+    return temp[0]
+
+tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+evalRPN(tokens)
