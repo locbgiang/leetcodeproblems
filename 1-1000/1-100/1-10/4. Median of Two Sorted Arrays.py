8bad968406ce2a1dce050bb805b5d2ef3ea00ab3
loc-giang <locbgiang@gmail.com>
2024-02-20 15:24:03 -0800

    4

--- 1-1000/1-100/1-10/4. Median of Two Sorted Arrays.py
+++ 1-1000/1-100/1-10/4. Median of Two Sorted Arrays.py
@@ -7,18 +7,18 @@ Explanation: merged array = [1,2,3] and median is 2.
 '''
 import math
 nums1 = [1,3]
-nums2 = [2]
+nums2 = [2,4]
 
 # Solution:
 def findMedianSortedArrays(nums1,nums2):
-    mergedArray = nums1 + nums2
-    mergedArray.sort()
+    new = nums1 + nums2
+    new.sort()
+    mid = (len(new)-1)/2
+    if len(new)%2 == 1 or len(new) <= 1:
+        answer = new[int(mid)]
+    else:
+        answer = (new[int(mid)] + new[int(mid+1)]) / 2.0
 
-    if len(mergedArray)%2 == 0:                         # if merged array is even length, add the two middle numbers and divide by 2
-        median = (mergedArray[int(len(mergedArray)/2-1)] + mergedArray[int(len(mergedArray)/2)])/2
-        return median
-    else:                                               # if odd length, just return the middle number
-        median = (mergedArray[math.floor(len(mergedArray)/2)])
-        return median
+    return answer
 # Solution
 print(findMedianSortedArrays(nums1, nums2))
